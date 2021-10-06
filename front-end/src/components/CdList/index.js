d6e74ae637192b042c1687f07068497d16000ffa
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-10-06 19:49:53 -0300

    added table in dashboard page with api data return

--- front-end/src/components/CdList/index.js
+++ front-end/src/components/CdList/index.js
@@ -1,15 +1,62 @@
-import React from 'react'
+import React, { useMemo, useState, useEffect } from 'react'
 
-const CdList = (props) => {
-  const list = props.itens;
+import Table from "../Table";
+
+import api from '../../services/api'
+
+import './styles.css';
+
+const CdList = () => {
+  const [data, setData] = useState([]);
+
+  const user_id = "ce4848a4-d765-4a72-bf34-eda0b3f21e41";
+
+  useEffect(() => {
+    (async () => {
+      const result = await api.post(`/cds/list/${user_id}`);
+
+      setData(result.data);
+    })();
+  }, []);
+
+  console.log(data)
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Lista de CD's",
+        columns: [
+          {
+            Header: "Name",
+            accessor: "name"
+          },
+          {
+            Header: "Artista",
+            accessor: "artist"
+          },
+          {
+            Header: "Ano de lançamento",
+            accessor: "release_year"
+          },
+          {
+            Header: "Gênero",
+            accessor: "genre"
+          },
+          {
+            Header: "Duração",
+            accessor: "duration"
+          }
+        ]
+      }
+    ],
+    []
+  );
 
   return (
-    <div className="cd-list">
-      <ul>
-        {list.map((cd) => <li key={cd.id}>{cd.artist} </li>)}
-      </ul>
+    <div className="App">
+      <Table columns={columns} data={data} />
     </div>
-  )
+  );
 }
 
 export default CdList
--- front-end/src/pages/Dashboard/index.js
+++ front-end/src/pages/Dashboard/index.js
@@ -1,25 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import CdList from '../../components/CdList';
-import api from '../../services/api'
+import React from 'react'
+
+import CdList from '../../components/CdList'
 
 import "./styles.css"
 
-const user_id = "ce4848a4-d765-4a72-bf34-eda0b3f21e41";
-
 const Dashboard = () => {
-  const [cd, setCd] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const { data } = await api.post(`/cds/list/${user_id}`);
-      setCd(data);
-      console.log(cd);
-    })();
-  }, []);
-
   return (
     <div>
-      <CdList itens={cd} />
+      <CdList />
     </div>
   )
 }
